6525972a132a8fb0f99e8e0abbea8d5025f4c5dc
Laura Neale <lauran@brandwatch.com>
2020-03-13 14:22:22 +0000

    multithreading but channel is None

--- chatbot.py
+++ chatbot.py
@@ -4,33 +4,38 @@ from time import sleep
 
 import discord
 from dotenv import load_dotenv
+import asyncio
 
 from questiondb import insert_question, select_question
 
 load_dotenv() #loads the .env file environment variables
 TOKEN = os.getenv('DISCORD_TOKEN')
-DEFAULT_CHANNEL = os.getenv('DISCORD_CHANNEL')
 
 client = discord.Client() #represents a connection to discord
-channel = client.get_channel(687940638314070057)
+channel_id = os.getenv('DISCORD_CHANNEL')
+DEFAULT_CHANNEL = client.get_channel(687940638314070057)
+
+print(channel_id)
+print(DEFAULT_CHANNEL)
 
 
 @client.event
 async def on_ready():  # when a connection to discord is established
     print(f'{client.user} has connected to Discord!')
+    client.loop.create_task(run_scheduled_questions())
 
 
 # TODO this doesn't work multi-threaded
 async def run_scheduled_questions():
     await client.wait_until_ready()
-    print(f'starting schedule on channel {channel}')
-    while True:
+    print(f'starting schedule on channel {DEFAULT_CHANNEL}')
+    while not client.is_closed():
         print("checking scheduling")
         hour = datetime.now().hour
-        if hour == 9 or hour == 16 or hour == 12:
+        if hour == 9 or hour == 16 or hour == 14:
             print("messaging on schedule")
-            await(send_question(channel))
-        sleep(600)
+            await(send_question(DEFAULT_CHANNEL))
+        await(asyncio.sleep(60))
 
 
 @client.event
@@ -53,5 +58,4 @@ async def send_question(channel):
     await(channel.send(response))
 
 
-# client.loop.create_task(run_scheduled_questions())
 client.run(TOKEN)  # runs the bot with the token
