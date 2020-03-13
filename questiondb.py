440de2358e4be28cab437f00b98a964f768c055f
Laura Neale <lauran@brandwatch.com>
2020-03-13 17:43:59 +0000

    order questions randomly

--- questiondb.py
+++ questiondb.py
@@ -4,7 +4,7 @@ import psycopg2
 def select_question():
     conn = connect_to_db()
     cur = conn.cursor()
-    cur.execute("SELECT question FROM discord_chatbot_questions ORDER BY last_asked NULLS FIRST, user_submitted DESC, question")
+    cur.execute("SELECT question FROM discord_chatbot_questions ORDER BY last_asked NULLS FIRST, user_submitted DESC, random()")
     qs = cur.fetchone()
     question = qs[0]
     cur.execute("UPDATE discord_chatbot_questions SET last_asked = now() WHERE question = %s", (question,))
